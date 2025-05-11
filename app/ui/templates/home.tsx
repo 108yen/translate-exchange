9e7cb5373678e1aee4dc0bab3fc453b5cf419f6a
108yen <kazuking.1911@gmail.com>
2025-05-11 21:57:55 +0900

    feat(ui): added home component

--- app/ui/templates/home.tsx
+++ app/ui/templates/home.tsx
@@ -1,3 +1,41 @@
+import {
+  Card,
+  CardBody,
+  CardHeader,
+  Heading,
+  Input,
+  List,
+  ListItem,
+  Separator,
+  Textarea,
+  VStack,
+} from "@yamada-ui/react"
+
 export function Home() {
-  return <p>Hello World!</p>
+  return (
+    <VStack m="md" separator={<Separator />}>
+      <Card colorScheme="gray" variant="subtle">
+        <CardBody>
+          <List>
+            <ListItem>
+              <Input value="1 USドル" />
+            </ListItem>
+            <ListItem>
+              <Input value="1 円" />
+            </ListItem>
+          </List>
+        </CardBody>
+      </Card>
+
+      <Card variant="subtle">
+        <CardHeader>
+          <Heading size="md">英語 → 日本語</Heading>
+        </CardHeader>
+
+        <CardBody>
+          <Textarea placeholder="Hello world!" />
+        </CardBody>
+      </Card>
+    </VStack>
+  )
 }
